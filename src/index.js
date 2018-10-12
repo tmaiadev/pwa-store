63600174a2467777c106b53cff85fa623a69950e
Thalles Maia <tmaiadev@gmail.com>
2018-10-12 12:41:36 -0300

    Make fonts load nicelly without blinking

--- src/index.js
+++ src/index.js
@@ -6,9 +6,3 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// import font asyncronally
-const $link = document.createElement('link');
-$link.rel = 'stylesheet';
-$link.href = 'https://fonts.googleapis.com/css?family=EB+Garamond';
-document.head.appendChild($link);
